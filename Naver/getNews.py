34f31e0d1862cb601febb7ac1643df701a277f62
bigMathGit <wkdrl9569@gmail.com>
2018-07-11 19:41:05 +0900

    update comment;

--- Naver/getNews.py
+++ Naver/getNews.py
@@ -32,17 +32,52 @@ class getNews:
         emails = pd.Series(emails)
         Media = pd.Series(soup.select('div.article_header > div.press_logo > a > img')[0].get_attribute_list('title')[0])
         area  = pd.Series(newsList[1])
-        grades = pd.Series(soup.select('div._reactionModule.u_likeit > a  > span.u_likeit_text._count.num')[0].get_text())
-        gain1 = pd.Series(soup.select('div._reactionModule.u_likeit > ul > li.u_likeit_list.good > a > span.u_likeit_list_count._count')[0].get_text())
-        gain2 = pd.Series(soup.select('div._reactionModule.u_likeit > ul > li.u_likeit_list.warm > a > span.u_likeit_list_count._count')[0].get_text())
-        gain3 = pd.Series(soup.select('div._reactionModule.u_likeit > ul > li.u_likeit_list.sad > a > span.u_likeit_list_count._count')[0].get_text())
-        gain4 = pd.Series(soup.select('div._reactionModule.u_likeit > ul > li.u_likeit_list.angry > a > span.u_likeit_list_count._count')[0].get_text())
-        gain5 = pd.Series(soup.select('div._reactionModule.u_likeit > ul > li.u_likeit_list.want > a > span.u_likeit_list_count._count')[0].get_text())
+        try:
+            grades = pd.Series(soup.select('div._reactionModule.u_likeit > a  > span.u_likeit_text._count.num')[0].get_text())
+            gain1 = pd.Series(soup.select('div._reactionModule.u_likeit > ul > li.u_likeit_list.good > a > span.u_likeit_list_count._count')[0].get_text())
+            gain2 = pd.Series(soup.select('div._reactionModule.u_likeit > ul > li.u_likeit_list.warm > a > span.u_likeit_list_count._count')[0].get_text())
+            gain3 = pd.Series(soup.select('div._reactionModule.u_likeit > ul > li.u_likeit_list.sad > a > span.u_likeit_list_count._count')[0].get_text())
+            gain4 = pd.Series(soup.select('div._reactionModule.u_likeit > ul > li.u_likeit_list.angry > a > span.u_likeit_list_count._count')[0].get_text())
+            gain5 = pd.Series(soup.select('div._reactionModule.u_likeit > ul > li.u_likeit_list.want > a > span.u_likeit_list_count._count')[0].get_text())
+        except:
+            grades = pd.Series('0')
+            gain1 = pd.Series('0')
+            gain2 = pd.Series('0')
+            gain3 = pd.Series('0')
+            gain4 = pd.Series('0')
+            gain5 = pd.Series('0')
         rank = pd.Series(newsList[2])
-        temp = DataFrame({'title' :title,'text':text,'wDate':wDate,'name':name,'emails':emails,'Media': Media,'area' :area,'grades' : grades,'gain1':gain1,'gain2':gain2,'gain3':gain3,'gain4':gain4,'gain5':gain5,'rank':rank},
+
+        try:
+            comment = pd.Series(soup.select('div.u_cbox_head > a > span.u_cbox_count')[0].get_text())
+            male = pd.Series(soup.select('div.u_cbox_chart_sex > div.u_cbox_chart_progress.u_cbox_chart_male > span.u_cbox_chart_per')[0].get_text())
+            female = pd.Series(soup.select('div.u_cbox_chart_sex > div.u_cbox_chart_progress.u_cbox_chart_female > span.u_cbox_chart_per')[0].get_text())
+            age1 =  pd.Series(soup.select('div.u_cbox_chart_age > div > span > span.u_cbox_chart_per ')[0].get_text())
+            age2 =  pd.Series(soup.select('div.u_cbox_chart_age > div > span > span.u_cbox_chart_per ')[1].get_text())
+            age3 =  pd.Series(soup.select('div.u_cbox_chart_age > div > span > span.u_cbox_chart_per ')[2].get_text())
+            age4 =  pd.Series(soup.select('div.u_cbox_chart_age > div > span > span.u_cbox_chart_per ')[3].get_text())
+            age5 =  pd.Series(soup.select('div.u_cbox_chart_age > div > span > span.u_cbox_chart_per ')[4].get_text())
+            comments = soup.select('div.u_cbox_comment_box > div.u_cbox_area > div.u_cbox_text_wrap > span.u_cbox_contents')
+            comms = ''
+            for i,comm in enumerate(comments):
+                comms = comms + str(i+1) + '. '+ str(comm.get_text()) + '\n'
+            comments = pd.Series(comms)
+        except:
+            comment = pd.Series('')
+            male= pd.Series('')
+            female= pd.Series('')
+            age1= pd.Series('')
+            age2= pd.Series('')
+            age3= pd.Series('')
+            age4= pd.Series('')
+            age5= pd.Series('')
+            comments= pd.Series('')
+
+
+        temp = DataFrame({'title' :title,'text':text,'wDate':wDate,'name':name,'emails':emails,'Media': Media,'area' :area,'grades' : grades,'gain1':gain1,'gain2':gain2,'gain3':gain3,'gain4':gain4,'gain5':gain5,'rank':rank,'comment' :comment,'male':male,'female':female,'age1':age1,'age2':age2,'age3': age3,'age4' :age4,'age5' : age5, 'comments' : comments},
                          columns=['title', 'text', 'wDate', 'name', 'emails', 'Media', 'area', 'grades', 'gain1', 'gain2', 'gain3',
-                 'gain4', 'gain5', 'rank'])
-        print(len(result))
+                 'gain4', 'gain5', 'rank', 'comment', 'male', 'female', 'age1', 'age2', 'age3', 'age4', 'age5', 'comments'])
+
 
         if len(result) == 0 :
             result = temp
--- Naver/rankNews.py
+++ Naver/rankNews.py
@@ -12,26 +12,25 @@ getNews = getNews.getNews()
 
 
 ## get Detail new
-def getDetailNews(result,result_comment,newsList):
+def getDetailNews(result,newsList):
     for i in range(len(newsList)):
-        #result = getNews.NewsDetail(result,newsList[i])
-        result_comment = getNews.NewsComment(result_comment,newsList[i])
+        result = getNews.NewsDetail(result,newsList[i])
+        # result_comment = getNews.NewsComment(result_comment,newsList[i])
     return result
 
 
-#sectionId = [100,101,102,103,104,105]
-num = 3
-sectionId = [101]
+sectionId = [100,101,102,103,104,105]
+num = 30
+scrapingDate = "20180711"
 
-scrapingDate = "20180702"
 getNews.open()
-result = pd.DataFrame(columns=['title', 'text', 'wDate', 'name', 'emails', 'Media', 'area', 'grades', 'gain1', 'gain2', 'gain3','gain4', 'gain5', 'rank'])
-result_comment = pd.DataFrame(columns=['comment','male', 'female', 'age1','age2','age3','age4','age5'])
+result = pd.DataFrame(columns=['title', 'text', 'wDate', 'name', 'emails', 'Media', 'area', 'grades', 'gain1', 'gain2', 'gain3','gain4', 'gain5', 'rank','comment','male', 'female', 'age1','age2','age3','age4','age5','comments'])
+# result_comment = pd.DataFrame(columns=['comment','male', 'female', 'age1','age2','age3','age4','age5'])
 
 
 for section in sectionId:
     newsList = getNews.getRank(section, num, scrapingDate)
-    result = getDetailNews(result,result_comment,newsList)
-    print('pd :', result)
+    result = getDetailNews(result,newsList)
+    print('pd :', result.tail(2))
 getNews.close()
 result.to_csv('result.csv', index=False)
